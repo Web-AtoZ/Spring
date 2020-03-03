1accec93cb81fbec55573b304bfa5921c451b6b0
smkim08 <smkim08@cafe24corp.com>
2020-03-03 17:27:37 +0900

    Spring-4 LogBack설정하기
    작업 완료

--- src/main/java/com/test/hello/controller/HelloController.java
+++ src/main/java/com/test/hello/controller/HelloController.java
@@ -1,14 +1,18 @@
 package com.test.hello.controller;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@Slf4j
 public class HelloController{
 
     @GetMapping("/")
     public String createApp()  {
+        log.info("Hello");
+        log.error("error");
         return "docker test !!!!";
     }
 
