a9c4b6902c66ae483f3bf0988f91d4e07c83246a
Yujaehyeong <mynameisyjh@gmail.com>
2020-03-04 00:01:27 +0900

    jackson-property-strategy

--- src/main/java/com/test/hello/interfaces/HelloController.java
+++ src/main/java/com/test/hello/interfaces/HelloController.java
@@ -6,13 +6,18 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HelloController {
 
-  @GetMapping("/")
-  public String createApp() {
-    return "docker test !!!!";
-  }
+    @GetMapping("/")
+    public String createApp() {
+        return "docker test !!!!";
+    }
 
-  @GetMapping("/hello")
-  public String hello() {
-    return "hello! docker test - update test";
-  }
+    @GetMapping("/hello")
+    public String hello() {
+        return "hello! docker test - update test";
+    }
+
+    @GetMapping("/students")
+    public Student getStudents() {
+        return new Student("john", 21, "010-9999-0000");
+    }
 }
