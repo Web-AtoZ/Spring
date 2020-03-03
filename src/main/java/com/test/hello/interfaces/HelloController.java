8e263320d48882b292f0687ffda70ce70f06a065
Soobinnn <isb9082@naver.com>
2020-03-03 21:25:17 +0900

    코드 리포멧팅 적용

--- src/main/java/com/test/hello/interfaces/HelloController.java
+++ src/main/java/com/test/hello/interfaces/HelloController.java
@@ -4,16 +4,15 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-public class HelloController{
+public class HelloController {
 
-    @GetMapping("/")
-    public String createApp()  {
-        return "docker test !!!!";
-    }
+  @GetMapping("/")
+  public String createApp() {
+    return "docker test !!!!";
+  }
 
-    @GetMapping("/hello")
-    public String hello()  {
-        return "hello! docker test - update test";
-    }
- 
+  @GetMapping("/hello")
+  public String hello() {
+    return "hello! docker test - update test";
+  }
 }
