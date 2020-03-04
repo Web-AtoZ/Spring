5ea6bf8ba1d8fa696f4c6b8402b6af5ca5e97453
hanseonghye <hans12312@naver.com>
2020-03-04 22:43:13 +0900

    pull reqest test

--- src/main/java/com/test/hello/interfaces/HelloController.java
+++ src/main/java/com/test/hello/interfaces/HelloController.java
@@ -15,4 +15,9 @@ public class HelloController {
   public String hello() {
     return "hello! docker test - update test";
   }
+
+  @GetMapping("/hello2")
+  public String hello2() {
+    return "hello! docker test - update test";
+  }
 }
