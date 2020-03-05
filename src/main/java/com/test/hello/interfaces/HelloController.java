e2ff1b8d66248f9a1ef80954225515cabe18357b
MaximSungmo <ksm5318@naver.com>
2020-03-05 21:29:27 +0900

    master merge

--- src/main/java/com/test/hello/interfaces/HelloController.java
+++ src/main/java/com/test/hello/interfaces/HelloController.java
@@ -10,21 +10,4 @@ public class HelloController {
     public String createApp() {
         return "docker test !!!!";
     }
-
-
-    @GetMapping("/hello")
-    public String hello() {
-        return "hello! docker test - update test";
-    }
-
-    @GetMapping("/students")
-    public Student getStudents() {
-        return new Student("john", 21, "010-9999-0000");
-    }
-  
-  @GetMapping("/hello2")
-  public String hello2() {
-    return "hello! docker test - update test";
-  }
-
 }
