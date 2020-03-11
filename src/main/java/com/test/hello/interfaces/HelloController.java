b0d16cd6d895a4d0b9df0b72e3bc48215b038d5e
=jungeunlee95 <=leeap1004@gmail.com>
2020-03-11 20:27:37 +0900

    add domain test

--- src/main/java/com/test/hello/interfaces/HelloController.java
+++ src/main/java/com/test/hello/interfaces/HelloController.java
@@ -1,7 +1,11 @@
 package com.test.hello.interfaces;
 
+import com.test.hello.domain.TestDomain;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.validation.Valid;
 
 @RestController
 public class HelloController {
@@ -9,5 +13,12 @@ public class HelloController {
   @GetMapping("/")
   public String createApp() {
     return "docker test !!!!";
+  }
+
+  @GetMapping("/annotation")
+  public TestDomain annotaionTest(
+          @Valid TestDomain testDomain
+  ) {
+    return testDomain;
   }
 }
