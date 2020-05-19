9df750589888dd0347b800ae314cd381a3275e48
Soobinnn <isb9082@naver.com>
2020-05-19 20:44:41 +0900

    Security 적용 추가

---
+++ src/main/java/com/webatoz/backend/global/config/security/SecurityJavaConfig.java
@@ -0,0 +1,28 @@
+package com.webatoz.backend.global.config.security;
+
+import com.webatoz.backend.utils.JwtUtil;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityJavaConfig extends WebSecurityConfigurerAdapter {
+
+  @Override
+  protected void configure(HttpSecurity http) throws Exception {
+    http
+        .cors().disable()
+        .csrf().disable()
+        .formLogin().disable();
+
+    //  iframe 허용 시
+    //  .headers.frameOptions().disable();
+  }
+  //  @Bean
+  //  public JwtUtil() {
+  //    return new JwtUtil();
+  //  }
+}
